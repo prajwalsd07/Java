3ce3e49ea0067648d3bf4b19196f6207acfda5b3
prajwalsd07 <prajwalsd9@gmail.com>
2023-02-01 12:11:25 +0530

    Day 7 commit

---
+++ Java_codes/src/Day7/Strings.java
@@ -0,0 +1,13 @@
+package Day7;
+
+public class Strings {
+
+	public static void main(String[] args) {
+String s1 = "hi";
+String s2 = "hello";
+System.out.println(s1+s2);
+System.out.println(s1.concat(s2));
+
+	}
+
+}
